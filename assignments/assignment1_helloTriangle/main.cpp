33e6d4e2f30e6fbfcd6484fdac17420d21dc33bd
thomasKriv <thomas.krivoruchka@mymail.champlain.edu>
2023-09-05 12:47:49 -0400

    Triangle created

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,6 +8,23 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float vertices[9] = { -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0,  0.5, 0.0 };
+
+const char* vertexShaderSource = R"(
+	#version 450
+	layout(location = 0) in vec3 vPos;
+	void main(){
+		gl_Position = vec4(vPos,1.0);
+	}
+)";
+const char* fragmentShaderSource = R"(
+	#version 450
+	out vec4 FragColor;
+	void main(){
+		FragColor = vec4(0.0,0.0,1.0,1.0);
+	}
+)";
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -27,10 +44,77 @@ int main() {
 		return 1;
 	}
 
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Color attribute
+	/*glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(0)sizeof(float)*3));
+	glEnableVertexAttribArray(1);*/
+
+	//Create a new vertex shader object
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(vertexShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+	}
+
+	//Create a new fragment shader object
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+	}
+
+	unsigned int shaderProgram = glCreateProgram();
+	//Attach each stage
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//Link all the stages together
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("Failed to link shader program: %s", infoLog);
+	}
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		//glUseProgram(shader);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
