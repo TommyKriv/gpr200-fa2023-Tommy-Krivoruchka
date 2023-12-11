741a731bb9bbbf7a21f0a086a61b860ba14091be
thomasKriv <thomas.krivoruchka@mymail.champlain.edu>
2023-12-11 02:07:15 -0500

    raaaa

--- core/patchwork/model.cpp
+++ core/patchwork/model.cpp
@@ -1,4 +1,7 @@
+#pragma once
+
 #include "model.h"
+#include "../ew/external/stb_image.h"
 
 namespace patchwork
 {
@@ -127,5 +130,46 @@ namespace patchwork
             }
         }
         return textures;
+    }
+
+    unsigned int TextureFromFile(const char* path, const std::string & directory, bool gamma)
+    {
+        std::string filename = std::string(path);
+        filename = directory + '/' + filename;
+
+        unsigned int textureID;
+        glGenTextures(1, &textureID);
+
+        int width, height, nrComponents;
+        unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+        if (data)
+        {
+            GLenum format;
+            if (nrComponents == 1)
+                format = GL_RED;
+            else if (nrComponents == 3)
+                format = GL_RGB;
+            else if (nrComponents == 4)
+                format = GL_RGBA;
+
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            stbi_image_free(data);
+        }
+        else
+        {
+            printf("Texture failed to load at path: ");
+            printf(path);
+            stbi_image_free(data);
+        }
+
+        return textureID;
     }
 }
